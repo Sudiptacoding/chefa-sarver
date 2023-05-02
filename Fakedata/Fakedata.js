f1f0529a345cb66c6e4aa9422677002017462100
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-05-02 13:16:09 +0600

    Fakedata on

---
+++ Fakedata/Fakedata.js
@@ -0,0 +1,143 @@
+module.exports = [
+    {
+        "_id": "0282e0e58a5c404fbd15261f11c2ab6a",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+        "total_view": 488,
+        "recipes": {
+            "image": "https://i.ibb.co/fNpdMp3/product12-570x300.jpg",
+            "title": "Meat & Chicken"
+        },
+        "chef": {
+            "name": "Shannon Bennett",
+            "published_date": "2022-08-24 17:27:34",
+            "img": "https://i.ibb.co/Kxm54PF/master-chef-3.jpg",
+            "experience": 5,
+            "numberOf_recipes": 3,
+            "Likes": 5000
+        }
+    },
+    {
+        "_id": "0282e0e58a5c404fbd15261f11c2ab6a",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+        "total_view": 488,
+        "recipes": {
+            "image": "https://i.ibb.co/fNpdMp3/product12-570x300.jpg",
+            "title": "Meat & Chicken"
+        },
+        "chef": {
+            "name": "Shannon Bennett",
+            "published_date": "2022-08-24 17:27:34",
+            "img": "https://i.ibb.co/Kxm54PF/master-chef-3.jpg",
+            "experience": 5,
+            "numberOf_recipes": 3,
+            "Likes": 5000
+        }
+    },
+    {
+        "_id": "0282e0e58a5c404fbd15261f11c2ab6a",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+        "total_view": 488,
+        "recipes": {
+            "image": "https://i.ibb.co/fNpdMp3/product12-570x300.jpg",
+            "title": "Meat & Chicken"
+        },
+        "chef": {
+            "name": "Shannon Bennett",
+            "published_date": "2022-08-24 17:27:34",
+            "img": "https://i.ibb.co/Kxm54PF/master-chef-3.jpg",
+            "experience": 5,
+            "numberOf_recipes": 3,
+            "Likes": 5000
+        }
+    },
+    {
+        "_id": "0282e0e58a5c404fbd15261f11c2ab6a",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+        "total_view": 488,
+        "recipes": {
+            "image": "https://i.ibb.co/fNpdMp3/product12-570x300.jpg",
+            "title": "Meat & Chicken"
+        },
+        "chef": {
+            "name": "Shannon Bennett",
+            "published_date": "2022-08-24 17:27:34",
+            "img": "https://i.ibb.co/Kxm54PF/master-chef-3.jpg",
+            "experience": 5,
+            "numberOf_recipes": 3,
+            "Likes": 5000
+        }
+    },
+    {
+        "_id": "0282e0e58a5c404fbd15261f11c2ab6a",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+        "total_view": 488,
+        "recipes": {
+            "image": "https://i.ibb.co/fNpdMp3/product12-570x300.jpg",
+            "title": "Meat & Chicken"
+        },
+        "chef": {
+            "name": "Shannon Bennett",
+            "published_date": "2022-08-24 17:27:34",
+            "img": "https://i.ibb.co/Kxm54PF/master-chef-3.jpg",
+            "experience": 5,
+            "numberOf_recipes": 3,
+            "Likes": 5000
+        }
+    },
+    {
+        "_id": "0282e0e58a5c404fbd15261f11c2ab6a",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+        "total_view": 488,
+        "recipes": {
+            "image": "https://i.ibb.co/fNpdMp3/product12-570x300.jpg",
+            "title": "Meat & Chicken"
+        },
+        "chef": {
+            "name": "Shannon Bennett",
+            "published_date": "2022-08-24 17:27:34",
+            "img": "https://i.ibb.co/Kxm54PF/master-chef-3.jpg",
+            "experience": 5,
+            "numberOf_recipes": 3,
+            "Likes": 5000
+        }
+    },
+    {
+        "_id": "0282e0e58a5c404fbd15261f11c2ab6a",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+        "total_view": 488,
+        "recipes": {
+            "image": "https://i.ibb.co/fNpdMp3/product12-570x300.jpg",
+            "title": "Meat & Chicken"
+        },
+        "chef": {
+            "name": "Shannon Bennett",
+            "published_date": "2022-08-24 17:27:34",
+            "img": "https://i.ibb.co/Kxm54PF/master-chef-3.jpg",
+            "experience": 5,
+            "numberOf_recipes": 3,
+            "Likes": 5000
+        }
+    }
+
+]
